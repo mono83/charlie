9fb102d2c5efb3dd915d3a609589d6f34df324a6
Gotterdemarung <gotterdemarung@gmail.com>
2018-09-11 20:44:55 +0300

    more cases for versions parsing

--- parse/version_test.go
+++ parse/version_test.go
@@ -15,13 +15,14 @@ var parseVersionDataProvider = []struct {
 	{"0.2.12", charlie.Version{Major: "0", Minor: "2", Patch: "12"}},
 	{"3.1-beta", charlie.Version{Major: "3", Minor: "1", Label: "beta"}},
 	{"0.1", charlie.Version{Major: "0", Minor: "1"}},
+	{"v4.0.0-rc.1", charlie.Version{Major: "4", Minor: "0", Patch: "0", Label: "rc.1"}},
 }
 
 func TestParseVersion(t *testing.T) {
 	for _, data := range parseVersionDataProvider {
 		t.Run(data.Given, func(t *testing.T) {
 			r, ok := Version(data.Given)
-			if assert.True(t, ok) {
+			if assert.True(t, ok, "unable to parse "+data.Given) {
 				assert.Equal(t, data.Expected, *r)
 			}
 		})
