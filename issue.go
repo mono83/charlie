88fdaf626836d755f3ccb654bebeda0675bd0e0c
Gotterdemarung <gotterdemarung@gmail.com>
2018-09-11 20:15:50 +0300

    issue

--- issue.go
+++ issue.go
@@ -1,7 +1,9 @@
 package charlie
 
+// Issue describes an issue, resolved or implemented in release
 type Issue struct {
-	ID      string
-	Type    Type
-	Message string
+	ID         string
+	Type       Type
+	Components []string
+	Message    string
 }
