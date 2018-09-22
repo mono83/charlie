f0b0aca605e90ca67ad80e8836c567e97228f3b9
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2018-09-22 16:14:49 +0300

    Traversing github release pages

--- drivers/github-releases-api.go
+++ drivers/github-releases-api.go
@@ -2,6 +2,7 @@ package drivers
 
 import (
 	"errors"
+	"fmt"
 )
 
 // GitHubReleasesAPI is a driver, that obtains release information
@@ -9,15 +10,6 @@ import (
 //
 // For each obtained release callback function will be invoked
 // Execution will be stopped when callback returns null or there are no more releases
-//
-// Flow:
-// 1. Driver reads latest release and sends it to callback
-//    https://developer.github.com/v3/repos/releases/#get-the-latest-release
-//    Example: https://api.github.com/repos/facebook/react/releases/latest
-// 2. If there were no errors it fetches releases list
-//    https://developer.github.com/v3/repos/releases/#list-releases-for-a-repository
-// 3. TODO If there is still no error - it traverses across other pages
-//
 //
 // PS. TODO implement authentication
 func GitHubReleasesAPI(repository string, callback func(title, body string) error) error {
@@ -27,35 +19,24 @@ func GitHubReleasesAPI(repository string, callback func(title, body string) erro
 
 	// TODO validate repository name
 
-	// Reading latest release into JSON
-	var rel simplifiedReleaseInfo
-	err := IntoJSON(&rel)(Only200(HTTPGet("https://api.github.com/repos/" + repository + "/releases/latest")))
-	if err != nil {
-		return err
-	}
-
-	// Invoking callback
-	if err := callback(rel.Name, rel.Body); err != nil {
-		return err
-	}
-
 	// Reading list into JSON
 	var list []simplifiedReleaseInfo
-	err = IntoJSON(&list)(Only200(HTTPGet("https://api.github.com/repos/" + repository + "/releases")))
-	if err != nil {
-		return err
-	}
-
-	for _, r := range list {
-		if r.Name == rel.Name {
-			continue
-		}
-
-		if err := callback(r.Name, r.Body); err != nil {
+	page := 1
+	for ok := true; ok; ok = len(list) > 0{
+		url := fmt.Sprintf("https://api.github.com/repos/%s/releases?page=%d", repository, page)
+		err := IntoJSON(&list)(Only200(HTTPGet(url)))
+		if err != nil {
 			return err
 		}
-	}
 
+		for _, r := range list {
+			if err := callback(r.Name, r.Body); err != nil {
+				return err
+			}
+		}
+
+		page++
+	}
 	return nil
 }
 
