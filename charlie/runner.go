47c80fdbf68753b5b4a0d91bcf47bc9c278ba029
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2018-12-08 10:45:18 +0200

    publishers and subscribers for request channel

--- charlie/runner.go
+++ charlie/runner.go
@@ -1,24 +1,57 @@
 package main
 
 import (
-	"github.com/mono83/charlie/drivers"
+	"fmt"
 	"github.com/mono83/charlie/config/ini"
-	"time"
-	"github.com/mono83/charlie/process"
+	"github.com/mono83/charlie/drivers"
 	"github.com/mono83/charlie/parse"
+	"github.com/mono83/charlie/process"
+	"time"
 )
 
 /*
 	This is an example of different drivers/parsers configurations being attached to channel
- */
+*/
 func main() {
+
+	// Creating channel for incoming requests
+	requests := make(chan string)
+
+	// Imitating requests for changelog processing
+	setPeriodicRequests(requests, 15*time.Second, "react")
+	setPeriodicRequests(requests, 40*time.Second, "spring")
+
+	// Subscribing processors
 	config, err := ini.GetDefaultConfig()
-	if (err != nil) {
+	if err != nil {
 		panic("No configuration found")
 	}
 
 	githubDriver := drivers.GithubDriver{config.Auth.Github}
-	lastProcessed := time.Unix(0, 0) // TODO need to obtain lastProcessedValue somewhere
-	reactProcessor := process.GetReleaseProcessor(parse.ReactChangelog, process.PrintToConsole)
-	githubDriver.ApplyToReleasesLastProcessed("facebook/react", reactProcessor, lastProcessed)
+	lastProcessedTimes := make(map[string]time.Time)
+
+	for {
+		request := <-requests
+		switch request {
+		case "react":
+			fmt.Println("### Processing request for facebook/react")
+			lastProcessed, found := lastProcessedTimes[request]
+			if !found {
+				lastProcessed = time.Unix(0, 0)
+			}
+			githubDriver.ApplyToReleasesLastProcessed("facebook/react", process.GetReleaseProcessor(parse.ReactChangelog, process.PrintToConsole), lastProcessed)
+			lastProcessedTimes[request] = time.Now()
+		default:
+			fmt.Println("### Processing for", request, "is not implemented yet")
+		}
+	}
+}
+
+func setPeriodicRequests(requests chan string, period time.Duration, request string) {
+	go func() {
+		ticker := time.NewTicker(period)
+		for range ticker.C {
+			requests <- request
+		}
+	}()
 }
